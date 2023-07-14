8c4a8c6a1a1e6e11cee63419fc682c50082187b2
Sesh-Gallagher <t.ntwana@gmail.com>
2023-07-14 08:06:59 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to a standard line
+ * Return: 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Kor
+par, 2015-10-19 \n", stdout);
+return (1);
+}
