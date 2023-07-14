8d01a8c7802e259e8d7b625a386ddc5a47e094f8
Sesh-Gallagher <t.ntwana@gmail.com>
2023-07-14 03:39:33 -0700

    program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+ #include<stdio.h>
+/**
+ * main - Programming is like building a multilingual puzzle, followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
