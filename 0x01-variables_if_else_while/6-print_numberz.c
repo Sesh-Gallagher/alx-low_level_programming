cbc96b0e305a3fb9509daa202adb268d58dacdf1
Sesh-Gallagher <t.ntwana@gmail.com>
2023-07-16 04:21:11 -0700

    to 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program that prints numbers of base 10 from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int q;
+
+	q = 48;
+	while (q < 58)
+	{
+		putchar(q);
+		q++;
+	}
+	putchar('\n');
+	return (0);
+}
