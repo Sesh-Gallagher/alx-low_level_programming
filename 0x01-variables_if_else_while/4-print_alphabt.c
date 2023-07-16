a92e16f0ed66a8b06ddb4a25a121a7bc22837996
Sesh-Gallagher <t.ntwana@gmail.com>
2023-07-16 03:42:36 -0700

    alphabets without

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints letter of alphabet except q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int z = 97;
+
+	while (0 < 123)
+	{
+		if (z != 101 && z != 113)
+		{
+			putchar(z);
+		}
+		z++;
+	}
+	putchar(10);
+	return (0);
+}
