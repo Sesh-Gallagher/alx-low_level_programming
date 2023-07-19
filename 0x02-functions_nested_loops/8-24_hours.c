24a06362e342e45ba717468cf65eddcbfebedb32
Sesh-Gallagher <t.ntwana@gmail.com>
2023-07-18 20:39:05 -0700

    jack_bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *jack_bauer - print evry minute of jacks's day starting form
+ * starting from 0:00 - 23:00
+ *
+ */
+void jack_bauer(void)
+{
+	int b, c;
+
+	b = 0;
+
+	while (b < 24)
+	{
+		c = 0;
+
+		while (b < 60)
+		{
+			_putchar((b / 10) + '0');
+			_putchar((b / 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+			c++;
+		}
+		b++;
+	}
+
+}
