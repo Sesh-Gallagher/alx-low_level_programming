586e51bae78799abaa7ed138d8f96635316f2efe
Sesh-Gallagher <t.ntwana@gmail.com>
2023-08-03 19:32:26 -0700

    fix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,22 +2,21 @@
 
 /**
  * wildcmp - Compare strings and return 1 if identical
- * @s1: pointer to string thats compared
- * @s2: pointer to string thats compared to first string
+ * @sum1: pointer to string thats compared
+ * @sum2: pointer to string thats compared to first string
  * Return: 0
  */
 
 int wildcmp(char *sum1, char *sum2)
 {
-        if (*sum1 == '\0')
-        {
-                if (*sum2 != '\0' && *sum2 == '*')
-                {
-                        return (wildcmp(sum1, sum2 + 1));
-                }
-                return (*sum2 == '\0');
-        }
-
+	if (*sum1 == '\0')
+	{
+		if (*sum2 != '\0' && *sum2 == '*')
+		{
+			return (wildcmp(sum1, sum2 + 1));
+		}
+		return (*sum2 == '\0');
+	}
 	if (*sum2 == '*')
 	{
 		return (wildcmp(sum1 + 1, sum2) || wildcmp(sum1, sum2 + 1));
