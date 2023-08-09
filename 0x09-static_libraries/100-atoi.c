0cc7d28f586547a1c3a53cc3b0cb3e45bc63e7fc
Sesh-Gallagher <t.ntwana@gmail.com>
2023-08-08 19:00:08 -0700

    Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory.

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -7,40 +7,24 @@
  *
  * Return: A integer
  */
+
 int _atoi(char *s)
 {
-	int a, b, c, len, f, digit;
+	int sign = 1, i = 0;
+	unsigned int res = 0;
 
-	a = 0;
-	b = 0;
-	c = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (a < len && f == 0)
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
-		if (s[a] == '-')
-			++b;
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			digit = s[a] - '0';
-			if (b % 2)
-				digit = -digit;
-				c = c * 10 + digit;
-				f = 1;
-				if (s[a + 1] < '0' || s[a + 1] > '9')
-					break;
-				f = 0;
-		}
-		a++;
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
 	}
-	if (f == 0)
-
-		return (0);
-
-	return (c);
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	res *= sign;
+	return (res);
 }
+
