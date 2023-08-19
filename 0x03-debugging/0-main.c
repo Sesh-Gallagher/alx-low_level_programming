adad2a3b1ecc4bd55dd955ca84e36cfd38e28203
Sesh-Gallagher <t.ntwana@gmail.com>
2023-08-19 07:42:23 -0700

    Debugging fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-
 /**
 * main - Test function for positive or negative
 * Return: 0
+*
 */
-
 
 int main(void)
 {
@@ -13,7 +12,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
-
