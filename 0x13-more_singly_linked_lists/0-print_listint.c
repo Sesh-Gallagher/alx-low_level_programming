cf1b56192ddf2463ddbb5bdba54a74cea3895fb7
Sesh-Gallagher <t.ntwana@gmail.com>
2023-08-29 19:34:29 -0700

    fix function that returns the number of elements in a linked listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 
 /**
  * print_listint - a function that prints all the elements of a linked list.
- * @h: The linked list head
+ * @h: The linked list type to print
  *
  * Return: the number of nodes
  *
@@ -14,13 +14,14 @@ size_t print_listint(const listint_t *h)
 	int counter;
 
 	counter = 0;
+
 	if (h != NULL)
 	{
 		while (h)
 		{
 			printf("%d\n", h->n);
-			h = h->next;
 			counter++;
+			h = h->next;
 		}
 	}
 
