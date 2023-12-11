54c1f6221d5cdb654c9936c2bb514a64b176cec3
Sesh-Gallagher <t.ntwana@gmail.com>
2023-12-11 07:59:45 -0800

    add note at the end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/*
+ * add_dnodeint_end - function that adds a new node at the end
+ * of a dlistint_t list.
+ * @head: pointer to the head of the end of the list
+ * @n: The integer node to be pal
+ *
+ * Return: the address of the new element, or NULL if it failed
+ *
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	dlistint_t *h;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->next = NULL;
+	new_node->n = n;
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+			h = h->next;
+		h->next = new_node;
+
+	}
+	else
+	{
+		*head = new_node;
+	}
+
+	new_node->prev = h;
+
+	return (new_node);
+}
+
