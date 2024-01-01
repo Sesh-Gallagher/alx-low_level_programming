c73b209f329cc50984f51a715051cc6ed0a49c5d
Sesh-Gallagher <t.ntwana@gmail.com>
2024-01-01 03:05:28 -0800

    djb2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str:  string to hash function or table.
+ *
+ * Return: The calculated hash function
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	int i;
+	unsigned long int hash;
+
+	hash = 5381;
+	while ((i = *str++))
+	{
+		hash = ((hash << 5) + hash) + i;
+	}
+	return (hash);
+}
