b9452a795270dac7194918d11ca1bd10ab4a83b5
root <t.ntwana@gmail.com>
2024-03-18 04:02:29 -0700

    free linked list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,31 +1,74 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a list safely
+ * free_listp2 - Function that frees a linked list
+ * @head: pointer to the head of a list.
  *
- * @h: head of list
- * Return: size of list free'd
+ * Return: none
  */
+
+void free_listp2(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - function that frees a list safely
+ * @h: pointer to the head of list
+ *
+ * Return: size of list to be freed
+ */
+
 size_t free_listint_safe(listint_t **h)
 {
-	size_t size = 0;
-	listint_t *slow, *fast;
+	listp_t *hptr, *new_node, *add_node;
+	listint_t *current;
+	size_t count = 0;
 
-	if (!*h)
-		return (0);
-
-	fast = *h;
-	while (fast)
+	hptr = NULL;
+	while (*h != NULL)
 	{
-		slow = fast;
-		fast = fast->next;
+		new_node = malloc(sizeof(listp_t));
 
-		size++;
-		free(slow);
+		if (new_node == NULL)
+			exit(98);
 
-		if (slow <= fast)
-			break;
+		new_node->p = (void *)*h;
+		new_node->next = hptr;
+		hptr = new_node;
+
+		add_node = hptr;
+
+		while (add_node->next != NULL)
+		{
+			add_node = add_node->next;
+			if (*h == add_node->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (count);
+			}
+		}
+
+		current = *h;
+		*h = (*h)->next;
+		free(current);
+		count++;
 	}
+
 	*h = NULL;
-	return (size);
+	free_listp2(&hptr);
+	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -38,6 +38,7 @@ typedef struct listint_s
 } listint_t;
 
 void free_listp(listp_t **head);
+void free_listp2(listp_t **head);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
